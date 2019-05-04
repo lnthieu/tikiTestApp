b6ae48b3d7413c8fbabe1980c9accbc825dfa3cd
lenguyetrunghieu <hieu.lenguyetrung@gmail.com>
2019-05-04 12:18:48 +0700

    Starting project.

---
+++ app/src/main/java/app/android/tiki/TikiTestApp.java
@@ -0,0 +1,18 @@
+package app.android.tiki;
+
+import android.app.Application;
+
+/**
+ * Created by hieu.lenguyentrung on 5/4/19.
+ */
+
+public class TikiTestApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Required initialization logic here!
+    }
+
+    
+}
